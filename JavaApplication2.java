dfbbf33aa6903b22cb8e9d471af85b79fd4dfce5
dandreb042697 <54787341+dandreb042697@users.noreply.github.com>
2019-09-18 10:57:36 +0800

    Add files via upload

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 public class JavaApplication2 {
 
-
     public static void main(String[] args) {
         Scanner dan = new Scanner(System.in);
-        System.out.print("Enter time in seconds :");
-   int sec = dan.nextInt(); 
-   int p1 = sec% 60;
-   int p2 = sec/ 60;
-   int p3 = p2 % 60;
-   p2 = p2 / 60;
-  System.out.println( "Time is "+p2 + " hour " + p3 + " minutes " + p1 +"seconds");
-    }
         
+        System.out.print("Enter three numbers:");
+        int number1 = dan.nextInt();
+        int number2 = dan.nextInt();
+        int number3 = dan.nextInt();
+        
+        if (number1>number2&&number1>number3){
+            System.out.println("Largest is "+number1);
+        }
+        else if (number2>number1&&number2>number3){
+            System.out.println("Largest is "+number2);
+        }
+        else if (number3>number1&&number3>number2){
+            System.out.println("Largest is "+number3);
+        }
+        
+    }
+    
 }
